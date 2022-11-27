4733d41e5cb310e0179c244170e694d790876131
Amscoo <85079072+Amscoo@users.noreply.github.com>
2022-11-27 21:55:26 +0100

    First Commit

---
+++ react_frontend/src/_nav.js
@@ -0,0 +1,22 @@
+export default {
+  items: [
+
+    {
+      name: 'Accueil',
+      url: '/starter',
+      icon: 'icon-speedometer',
+
+    },
+    {
+      name: 'Milliardaires Forbes',
+      url: '/csvdatareder',
+      icon: 'icon-user',
+
+    },
+    
+    {
+      divider: true,
+    },
+
+  ],
+};
